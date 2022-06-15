57c12de46414aebc71c3441265f143f275fa5f1f
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:46:59 +0700

    Edit js file to pass the test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = (string) => {
+  let pureString = string.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
+  let reverseString = pureString.toLowerCase().split('').reverse().join('');
+  if (pureString === reverseString) {
+    return true;
+  }else {
+    return false;
+  }
 };
 
 // Do not edit below this line

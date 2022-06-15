96da1fcaebee2539a28f7b8ae23b905a52188dad
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:44:40 +0700

    Edit js file to pass the test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = (f) => {
+  let c = (f - 32) * 5 / 9;
+  c = Math.round(c * 10) /10;
+  return c;
 
 };
 
-const ctof = function() {
+const ctof = (c) => {
+  let f = c * 9 / 5 + 32;
+  f = Math.round(f * 10) /10;
+  return f;
 
 };
 

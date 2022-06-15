987151443dc51782bb25c22e68be675017254a88
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:47:53 +0700

    Edit js file to pass the test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+//fibonacci is a0 , a1 = (a-1 + a0), a2 = (a0 + a1), a++;
+const fibonacci = (num) => {
+  num = Number(num);
+  let array = [];
+//starting point 
+  let nextNum = 0;
+  if (num < 0) {
+    return 'OOPS';
+  } else {
+//if 1 is the first value next value is going to be 1 + value preceding 1 which is null/ 0 return 0 + 1 = 1;
+//nextNUm = current index + previous index
+    for (let i = 0; i < num; i++) {
+      if ( array.length <= 1) {
+        nextNum = 1;
+        array.push(nextNum);
+    }   else {
+        nextNum = Number(array[array.length - 2]) + Number(array[array.length - 1])
+        array.push(nextNum);
+    } 
+    } return array[array.length - 1];
+  }
 };
 
 // Do not edit below this line

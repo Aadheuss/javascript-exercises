cfeaeb8f0481622c0cd9e186f1f48d7924f4006d
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:41:08 +0700

    Edit js file to pass the test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (firstArray,...toRemove) => {
+    let modifiedArray = [...firstArray];
+    for (i = 0; i < firstArray.length; i++) {
+        if (modifiedArray.includes(toRemove[i])) {
+            modifiedArray.splice(modifiedArray.indexOf(toRemove[i]), 1)
+        }    
+    }
+    return modifiedArray;
+}
+  
+// ([1, 2, 3, 4], 3) = ([1, 2, 4])
+//
+/*(array, n, n
+  keep the element inside array except if the outside array math value o) */
 
 // Do not edit below this line
 module.exports = removeFromArray;

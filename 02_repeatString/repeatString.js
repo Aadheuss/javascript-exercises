56e37ef04b8c84d09fc4ceeb995e735ddfe5e739
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:38:31 +0700

    Edit js file to pass the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let newString = '';
+  let i = 0;
+  do {
+    if (num < 0) {
+    return 'ERROR';
+    }else if (num === 0 || string == null)  {
+      return newString;
+    }else if (num === 1) {
+      return string;
+    }else {
+      newString += string;
+    }
+  i++ 
+  } 
 
-};
+  while (i < num);
+  return newString;
+}
+//string, num
+//repeatString = string times number 
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
+

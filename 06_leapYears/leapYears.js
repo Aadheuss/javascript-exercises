10db1682d445056da582fb8ceed50329246c80a7
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:43:22 +0700

    Edit js file to finish the test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = year => {
+  if (year % 400 === 0) {
+    return true;
+  }else if(year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  }else {
+    return false;
+  }
 
 };
+//if number is divisible by 4 it's a leap year
+//unless it's divisable by 100
+//but if it's divisable 
 
 // Do not edit below this line
 module.exports = leapYears;

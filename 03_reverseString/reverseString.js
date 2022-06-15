0f82e1e4e25d14e64cee8f5770b64fe0c07bcf20
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:39:43 +0700

    Edit js file to pass the test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
+const reverseString = string => string.split('').reverse().join('');
+;
 
 // Do not edit below this line
 module.exports = reverseString;
+//npm test reverseString.spec.js

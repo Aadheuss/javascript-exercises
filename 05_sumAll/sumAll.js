24402f45a9b81f4f6152d805d064f1de67ef6e4b
Aadheuss <aadheusx@gmail.com>
2022-06-15 19:42:01 +0700

    Edit js file to pass the test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
+  let total = 0;
+  console.log(typeof(b));
+  if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  } else if (a < b) {
+    for (let i = a; i <= b; i++) {
+    total += i;
+    } 
+  } else {
+    for ( let i = b; i <= a; i++) {
+      total += i;
+    } 
+  } return total;
 
-};
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
